2859850b601f0dc239d66e46b804b1bfa55cdc99
JohnnyCatsvill <56757938+JohnnyCatsvill@users.noreply.github.com>
2020-03-16 08:15:27 +0300

    Add files via upload

--- test.bat
+++ test.bat
@@ -1,9 +1,12 @@
 Debug\MatrixInverse.exe input.txt
-echo ERROR=%ERRORLEVEL%
+echo ERROR=%ERRORLEVEL% ? 0
 
 Debug\MatrixInverse.exe input2.txt
-echo ERROR=%ERRORLEVEL%
+echo ERROR=%ERRORLEVEL% ? 2
 
 Debug\MatrixInverse.exe
-echo ERROR=%ERRORLEVEL%
+echo ERROR=%ERRORLEVEL% ? 3
+
+Debug\MatrixInverse.exe noinput.txt
+echo ERROR=%ERRORLEVEL% ? 1
 pause
